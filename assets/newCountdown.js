58a2214ab8daf5468ad3cf5d3beeb957fe25eee7
jonaaldas <jonaaldas@gmail.com>
2022-12-27 18:55:49 -0500

    added new files

--- assets/newCountdown.js
+++ assets/newCountdown.js
@@ -1,25 +1,27 @@
-// let time = '12-30-2024 16:00';
-// let startCountDown = (time) => {
-// 	let startTimer = () => {
-// 		let now = new Date().getTime();
-// 		let countDownDate = new Date(`${time}`).getTime();
-// 		let distance = countDownDate - now;
-// 		let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-// 		let hours = Math.floor(
-// 			(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-// 		);
-// 		let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-// 		let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+let time;
+let startCountDown = (time) => {
+	let startTimer = () => {
+		let now = new Date().getTime();
+		let countDownDate = new Date(`${time || '12-30-2024 16:00'}`).getTime();
+		let distance = countDownDate - now;
+		let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		let hours = Math.floor(
+			(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+		);
+		let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-// 		document.getElementById('demo').innerHTML =
-// 			days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
+		document.querySelector('.inner-days').innerText = days;
+		document.querySelector('.inner-hour').innerText = hours;
+		document.querySelector('.inner-min').innerText = minutes;
+		document.querySelector('.inner-sec').innerText = seconds;
 
-// 		if (distance < 0) {
-// 			clearInterval(x);
-// 			document.getElementById('demo').innerHTML = 'EXPIRED';
-// 		}
-// 	};
+		if (distance < 0) {
+			clearInterval(startTimer);
+			document.getElementById('demo').innerHTML = 'EXPIRED';
+		}
+	};
 
-// 	setInterval(startTimer, 1000, time);
-// };
-// export default startCountDown(time);
+	setInterval(startTimer, 1000, time);
+};
+startCountDown(time);
